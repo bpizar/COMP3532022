a188ac06ce6d4c0ddb4f1b6fd43d7001bf47a75c
Sam Mojaverian <sammojaverian@Sams-MacBook-Pro.local>
2022-07-29 16:20:58 -0400

    init.sql is Benjamin's tables with Pablo's modification, dropAll does what its name says, drops all the tables in the correct order

---
+++ Final Project/dropAll.sql
@@ -0,0 +1,18 @@
+DROP TABLE 
+emails,
+subscribed, 
+article, 
+authors, 
+organizations, 
+administrator, 
+regularUser, 
+orgDel, 
+researchers, 
+isSuspended, 
+users, 
+vaccineStat, 
+vaccines, 
+covid_19stat, 
+prostater, 
+country,
+region;
