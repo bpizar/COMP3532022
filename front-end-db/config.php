98e4b711aa6b418749c2d223961a1eed2dff05dc
Sam Mojaverian <sammojaverian@Sams-MacBook-Pro.local>
2022-07-29 22:00:44 -0400

    removed my own db password, just change it to yours when u want to use

--- front-end-db/config.php
+++ front-end-db/config.php
@@ -7,7 +7,7 @@
 
 $host       = "127.0.0.1";
 $username   = "root";
-$password   = "agnifyra42";
+$password   = "password"; //put your password
 $dbname     = "ProjectC19Main"; // will use later
 $dsn        = "mysql:host=$host;port=$port,dbname=$dbname"; // will use later
 $options    = array(
