448ab2573dd3e368cbcf37728ae95233df62f814
Sam Mojaverian <sammojaverian@Sams-MacBook-Pro.local>
2022-07-30 12:19:38 -0400

    removed my pw

--- front-end-db/config.php
+++ front-end-db/config.php
@@ -7,7 +7,7 @@
 
 $host       = "127.0.0.1";
 $username   = "root";
-$password   = "agnifyra42"; //put your password
+$password   = "putYourPW"; //put your password
 $dbname     = "ProjectC19Main"; // will use later
 $port = "3306";
 $dsn        = "mysql:host=$host;port=$port;dbname=$dbname"; // will use later
