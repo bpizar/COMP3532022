e2bc36d03a54dd45fce64cf48dff6c49eb34be7f
Sam Mojaverian <=>
2022-08-03 10:04:12 -0400

    email generation

--- front-end-db/createArticle.php
+++ front-end-db/createArticle.php
@@ -35,6 +35,43 @@ try {
     die("ERROR: Could not able to execute $sql. " . $e->getMessage());
 }
 
+try {
+    $trigger = $connection->prepare("SELECT email, users.uID FROM users INNER JOIN subscribed ON users.uID = subscribed.uID WHERE subscribed.authorID = :authorID");
+
+
+    $trigger->bindParam(':authorID', $_REQUEST['authorID']);
+
+    $trigger->execute();
+
+    $data = $trigger->fetchAll();
+
+    foreach ($data as $row) {
+        $to = $row['email'];
+        $subject = 'New Article from ' . $_REQUEST['Author'] . ' topics are ' . $_REQUEST['majorTopic'] . ' and ' . $_REQUEST['minorTopic'];
+        $message = 'An Article you subscribed to as been published, here is the summary of the article' . $_REQUEST['summary'];
+        $from = 'zuc353_1@encs.concordia.ca';
+
+        $date = date('d-m-y h:i:s');
+        $email = $connection->prepare("INSERT INTO emails(uID, authorID, dateTime, subject)  VALUES (:uID, :authorID, :dateTime, :subject)");
+
+        $email->bindParam(':uID', $row['uID']);
+        $email->bindParam(':authorID', $_REQUEST['authorID']);
+        $email->bindParam(':dateTime', $date);
+        $email->bindParam(':subject', $subject);
+
+        $email->execute();
+
+        // Sending email
+        if (mail($to, $subject, $message)) {
+            echo 'Your mail has been sent successfully.';
+        } else {
+            echo 'Unable to send email. Please try again.';
+        }
+    }
+} catch (PDOException $e) {
+    die("ERROR: Could not able to execute $sql. " . $e->getMessage());
+}
+
 // Close connection
 unset($connection);
 
